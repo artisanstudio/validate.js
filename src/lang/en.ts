15be0a1c9fd3320841f7d3c500f6868c877f827d
Jaggy Gauran <jaggy@artisan.studio>
2019-02-10 21:41:37 +0800

    Fix the email error message.

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -1,4 +1,4 @@
 export default {
   required: 'The :attribute field must be required.',
-  email: 'The :attribute field must be an email address.'
+  email: 'The :attribute must be a valid email address.'
 }
