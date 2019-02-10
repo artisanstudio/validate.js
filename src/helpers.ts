9ce9db4003e74483bc8ac63a1b3a2a865846fe17
Jaggy Gauran <jaggy@artisan.studio>
2019-02-10 17:44:08 +0800

    Export the main files.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,3 @@
-import Validator from './Validator'
-
 export const arrayWrap = (value: any) => {
   if (Array.isArray(value)) {
     return value
---
+++ src/validate.ts
@@ -0,0 +1,4 @@
+import Validator from './Validator'
+import Lang from './Lang'
+
+export { Validator, Lang }
