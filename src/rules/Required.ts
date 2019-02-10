4362157698d2596eafec67665a969632dba9e9db
Jaggy Gauran <jaggy@artisan.studio>
2019-02-10 11:20:36 +0800

    Add a custom validation message.

--- src/rules/Required.ts
+++ src/rules/Required.ts
@@ -2,6 +2,10 @@ class Required {
     passes (attribute: string, value: any) {
         return value !== undefined && value !== null && value !== ""
     }
+
+    message (attribute: string) {
+        return `The ${attribute} field must be required.`
+    }
 }
 
 export default Required
--- test/rules/Required.test.ts
+++ test/rules/Required.test.ts
@@ -4,9 +4,23 @@ describe("Validator", () => {
   it("returns false if the value is null or undefined", () => {
     const rule = new Required()
 
-    expect(rule.passes('email', undefined)).toBe(false)
-    expect(rule.passes('email', "")).toBe(false)
-    expect(rule.passes('email', null)).toBe(false)
+    expect(rule.passes('name', undefined)).toBe(false)
+    expect(rule.passes('name', "")).toBe(false)
+    expect(rule.passes('name', null)).toBe(false)
+  })
+
+  it("returns true of the value exists", () => {
+    expect(
+      new Required().passes('name', "Some Name")
+    ).toBe(true)
+  })
+
+  it("return the validation message", () => {
+    expect(
+      new Required().message("name")
+    ).toBe(
+      "The name field must be required."
+    )
   })
 })
 
